d46ab6fb902fee1a9b4c9d30d058820dec60b21e
Phi Trinh <phi@Phis-Mac.local>
2017-07-05 10:25:41 +0700

    Load image

--- downloadImage/ViewController.swift
+++ downloadImage/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textImange: UIImageView!
+    @IBOutlet weak var textLink: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnLoadImage(_ sender: UIButton) {
+        if textLink.text == "" {
+        let alert = UIAlertController(title: "Error", message:"please input link image", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            let url = textLink.text
+            downloadImangeURL(url!)
+            
+        }
+        
+    }
+    
+    func downloadImangeURL(_ url: String) {
+        let imgURl = URL(string: url)
+        (URLSession(configuration: .default)).dataTask(with: imgURl!, completionHandler: {(dataImage, response, Error) in
+            if let data = dataImage {
+                DispatchQueue.main.async() { () -> Void in
+                    self.textImange.image = UIImage(data: data)
+                }
+            }
+    }).resume()
+    }
+    
+    @IBAction func btnClear(_ sender: Any) {
+        
+        textLink.text = ""
+        textImange.image = nil
+    }
 
 }
 
